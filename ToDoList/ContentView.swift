5129288e12658b6e9b7e3eba24ff83a87df925a3
funMango <138420832+funMango@users.noreply.github.com>
2024-01-08 13:03:48 +0900

    Installing Firestore and instantiating Firebase

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,8 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
 
 struct ContentView: View {
+    
+    private let db: Firestore
+    
+    init() {
+        self.db = Firestore.firestore()
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,6 +28,6 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView(db: <#Firestore#>)
+//}
--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -23,6 +23,7 @@ struct ToDoListApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      
     FirebaseApp.configure()
     print("Configured Firebase!")
 
